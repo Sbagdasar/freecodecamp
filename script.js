f055f55cdc075fe30ea46abde6d61e71f120301b
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-26 23:45:13 +0300

    added task 6

--- script.js
+++ script.js
@@ -89,7 +89,7 @@
     return answer;
   }
   
-  chainToSwitch(7);*/
+  chainToSwitch(7);
   // task 5
   function isLess(a, b) {
     // Only change code below this line
@@ -98,4 +98,19 @@
     // Only change code above this line
   }
   
-  isLess(10, 15);
+  isLess(10, 15);*/
+  // task 6
+  // Setup
+function abTest(a, b) {
+    // Only change code below this line
+  
+  if(a<0 || b<0){
+    return undefined
+  }
+  
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
