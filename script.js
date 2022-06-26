b067b4a91d2d2b821ae7eca37408c8c586fa7866
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-26 23:28:32 +0300

    debug task

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function caseInSwitch(val) {
     let answer = "";
     // Only change code below this line
   switch (val) {
-    case '1':"alpha"
+    case 1:answer="alpha";
       break;
-    case "2": "beta"
+    case 2: answer="beta";
       break;
-      case "3": "gamma"
+      case 3: answer="gamma";
       break;
-      case "4": "gamma"
-      break;
+      case 4: answer="delta";
   }
   
   
