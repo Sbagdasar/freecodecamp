5e0ec89c49b1f1ba89c4e54483c9a957304f4712
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-26 23:36:04 +0300

    added task 3

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
   caseInSwitch(1);
   */
  //task 2
- function switchOfStuff(val) {
+ /*function switchOfStuff(val) {
     let answer = "";
     // Only change code below this line
   switch(val){
@@ -39,3 +39,29 @@
   }
   
   switchOfStuff(1);
+  */
+ // task 3
+ function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+    case 1:
+    case 2:
+    case 3: answer = "Low";
+    break;
+    case 4:
+    case 5:
+    case 6: answer = "Mid";
+    break;
+    case 7:
+    case 8:
+    case 9: answer = "High";
+    break;
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  sequentialSizes(1);
