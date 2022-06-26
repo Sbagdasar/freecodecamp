304156d0a7a2f674ceb8ce4f65dff7a8181d08f9
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-26 23:25:03 +0300

    added task 1

---
+++ script.js
@@ -0,0 +1,20 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+    case '1':"alpha"
+      break;
+    case "2": "beta"
+      break;
+      case "3": "gamma"
+      break;
+      case "4": "gamma"
+      break;
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
