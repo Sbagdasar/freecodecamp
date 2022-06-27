490275a7c393a40d1fb342fc66b6a17e9a7b87d4
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-27 23:29:35 +0300

    added task 9

--- script.js
+++ script.js
@@ -155,3 +155,22 @@ const myDog = {
   
     // Only change code above this line
   };
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    const lookup ={
+        "alpha":"Adams",
+        "bravo":"Boston",
+        "charlie":"Chicago",
+        "delta":"Denver",
+        "echo":"Easy",
+        "foxtrot":"Frank"
+    }
+    result = lookup[val]
+    // Only change code above this line
+    return result;
+}
+
+phoneticLookup("charlie");
