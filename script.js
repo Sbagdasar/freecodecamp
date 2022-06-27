5064655366be2193810b454ebdf99b865e6d0fdd
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-28 00:56:00 +0300

    added new task

--- script.js
+++ script.js
@@ -278,3 +278,16 @@ for(let i=0; i<myArr.length; i++){
     total+= myArr[i]
 }
 // Only change code below this line
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for(let i=0; i<arr.length; i++){
+        for(let j=0; j<arr[i].length; j++){
+            product=arr[i][j]*product
+        }
+    }
+    // Only change code above this line
+    return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
