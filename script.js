bfa0a24d2607d883edbe71328e004e469ced9336
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-28 22:21:42 +0300

    added new task

--- script.js
+++ script.js
@@ -323,16 +323,17 @@ const contacts = [
 
 function lookUpProfile(name, prop) {
     // Only change code below this line
-    for(let i=0;i<contacts.length;i++){
-        if(name === contacts[i].firstName && contacts[i].hasOwnProperty(prop)){
-            return contacts[i][prop]
-        }else if(name !== contacts[i].firstName){
-            return "No such contact"
-        }else if(contacts[i].hasOwnProperty(prop) === false){
-            return "No such property"
+    for(let i=0;i<contacts.length;i++) {
+        if (name === contacts[i].firstName) {
+            if (contacts[i].hasOwnProperty(prop)) {
+
+                return contacts[i][prop];
+            } else {
+                return "No such property"
+            }
         }
     }
-    // Only change code above this line
+        return "No such contact"
+        // Only change code above this line
 }
-
 console.log(lookUpProfile("Kristian", "lastName"))
