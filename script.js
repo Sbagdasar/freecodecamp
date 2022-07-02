c1a5976cddbab0817de2ad8d83018a797e03ec8e
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-02 23:33:06 +0300

    added new task

--- script.js
+++ script.js
@@ -363,7 +363,7 @@ function checkSign(num) {
     return (num > 0)?"positive":(num<0)?"negative":"zero"
 }
 
-checkSign(10);*/
+checkSign(10);*//*
 const countArray =[]
 function countup(n) {
 
@@ -390,4 +390,20 @@ function rangeOfNumbers(startNum, endNum) {
     }
 
 };
-console.log(rangeOfNumbers(1, 5))
+//console.log(rangeOfNumbers(1, 5))*/
+function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS)
+
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
