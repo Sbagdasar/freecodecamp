02769ab758d311be9bce95e5203decee703ac3fc
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-04 00:02:21 +0300

    added new task

--- script.js
+++ script.js
@@ -443,6 +443,13 @@ const HIGH_TEMPERATURES = {
 // Only change code below this line
 
 const {today,tomorrow} = HIGH_TEMPERATURES;
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+};
 
+// Only change code below this line
 
+const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
 // Only change code above this line
