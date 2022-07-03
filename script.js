65a15cfa1213f0721808ebe9a715976f9fa940c2
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-03 23:55:35 +0300

    added new task

--- script.js
+++ script.js
@@ -427,3 +427,9 @@ const sum = (...args) => {
     return args.reduce((a, b) => a + b, 0);
 }
 sum(1,2,3,4,5,5)
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
