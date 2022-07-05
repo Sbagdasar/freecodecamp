edef08858a5b4cf1eecbdd7097ac5445c542846f
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-05 22:54:05 +0300

    added new task

--- script.js
+++ script.js
@@ -471,8 +471,20 @@ const increment = (number, value=1) => number + value;
 // var str = 'John Smith';
 // var newstr = str.replace(re, '$2, $1');
 // console.log(newstr);
-function correct(string)
-{
-  return string.replace(/0/gi, "O").replace(/1/gi,"I").replace(/5/gi,"S")
-}
-console.log(correct("L0ND0N"))
+// function correct(string)
+// {
+//   return string.replace(/0/gi, "O").replace(/1/gi,"I").replace(/5/gi,"S")
+// }
+// console.log(correct("L0ND0N"))
+
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    const {today:{low:lowToday, high:highToday}}=LOCAL_FORECAST
+    
+  
+  // Only change code above this line
