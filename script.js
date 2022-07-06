9145d618b2dba8a5c5805270e718ef4ae0190aba
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-06 23:55:54 +0300

    added new task string restructur..

--- script.js
+++ script.js
@@ -510,3 +510,22 @@ function mergeArrays(arr1, arr2) {
   // Only change code below this line
   const half = ({max,min}) => (max + min) / 2.0; 
   // Only change code above this line
+
+
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for(let i=0; i<arr.length; i++){
+      failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
