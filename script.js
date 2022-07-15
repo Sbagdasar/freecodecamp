c072ee93605c7772e923588833c4272fc2e95963
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-15 20:12:09 +0300

    added new task

--- script.js
+++ script.js
@@ -453,3 +453,9 @@ const HIGH_TEMPERATURES = {
 
 const {today: highToday, tomorrow: highTomorrow} = HIGH_TEMPERATURES;
 // Only change code above this line
+
+import * as stringFunctions from './string_functions.js'
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
